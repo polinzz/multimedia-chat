5f1c5c01147f7585d9dc2088270dd8cfc686ab62
Doud75 <adrienquimbre@gmail.com>
2023-10-24 12:53:08 +0200

    connect db

--- back/server.js
+++ back/server.js
@@ -3,26 +3,27 @@ import process from "process";
 import userRoute from './routes/user.js';
 import convRoute from './routes/conv.js';
 import messageRoute from './routes/message.js';
+import fastifyPostgres from "@fastify/postgres";
 const fastify = Fastify({
   logger: true
 })
 
-/*fastify.register(require('@fastify/postgres'), {
-  connectionString: 'postgres://postgres@localhost/postgres'
-})*/
+fastify.register(fastifyPostgres, {
+  connectionString: 'postgres://user:password@127.0.0.1:5432/CHAT',
+});
 
 fastify.register(userRoute);
 fastify.register(convRoute);
 fastify.register(messageRoute);
 
-/*fastify.get('/user/:id', function (req, reply) {
+fastify.get('/user/:id', function (req, reply) {
   fastify.pg.query(
-    'SELECT id, username, hash, salt FROM users WHERE id=$1', [req.params.id],
+    'SELECT id, name FROM "user" WHERE id=$1', [req.params.id],
     function onResult (err, result) {
-      reply.send(err || result)
+      reply.send(result)
     }
   )
-})*/
+})
 
 try {
   await fastify.listen({ port: 4499 })
