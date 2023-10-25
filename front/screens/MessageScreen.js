9e9c87deefb671810793dd524ddb25f71fbbcc32
ValentineLefebvre <v.lefebvre.maths@gmail.com>
2023-10-25 21:48:54 +0200

    input en component

--- front/screens/MessageScreen.js
+++ front/screens/MessageScreen.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { NewMessage } from '../components/NewMessage.js';
+import NewMessage from '../components/NewMessage.js';
 export default function ({ route }) {
 	const { conversationId } = route.params;
-	const [myMessage, setMyMessage] = useState('');
 	return (
 		<View>
 			<Text>Conversation ID: {conversationId}</Text>
